af67bb59beea8cd29180858c43b6e80c3bde584f
kutsushisouseki-prog <kutsushisouseki@gmail.com>
2026-03-21 13:57:15 +0900

    Simplify webpack splitChunks configuration
    Removed unnecessary optimization settings for splitChunks in webpack configuration.

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -126,18 +126,6 @@ const config = {
     minimize: isProduction,
     splitChunks: {
       chunks: "all",
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName =
-              module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `vendor.${packageName.replace("@", "")}`;
-          },
-        },
-      },
     },
     usedExports: true,
   },
